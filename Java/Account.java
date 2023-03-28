263d84ae5237697ae6b297ec71493cfe97408a10
Kelvin <kelvin.h.suarez@gmail.com>
2023-03-28 15:27:21 +0200

    Java creation de objet

--- Java/Account.java
+++ Java/Account.java
@@ -1,9 +1,14 @@
 package Java;
 
-public class Account {
+class Account {
     Integer id;
     String name;
     String document;
     String email;
     String password;
+
+    public Account(String name, String document){
+        this.name = name;
+        this.document = document;
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,18 +1,14 @@
 package Java;
+
 class Main{
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
 
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Pereira";
+        Car car = new Car("AMQ123", new Account("Andres Pereira", "AND123"));
         car.passengers = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "OPC421";
-        car2.driver = "Maurice Pietre";
+        Car car2 = new Car("OPC421", new Account("Maurice Pietre", "BDS542"));
         car2.passengers = 4;
         car2.printDataCar();
 
